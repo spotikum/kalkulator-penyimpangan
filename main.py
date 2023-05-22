4dab94a324ac36701dca37d531a7b1139de1fc76
Indra Wahyu <wicaksana0701@gmail.com>
2023-05-23 04:13:35 +0800

    fix auto open notepad

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
+import os
+
+# Hapus data di hasil.txt
+with open('hasil.txt', 'w') as f:
+  f.write('')
+
+# Buka file kalkulator.txt dan arduino.txt
 with open('kalkulator.txt', 'r') as f1, open('arduino.txt', 'r') as f2:
-     line1 = f1.readline()
-     line2 = f2.readline()
-     while line1 and line2:
-          hasil = (float(line1) + float(line2))
+     kalkulator = f1.readline()
+     arduino = f2.readline()
+
+     # Mulai perhitungan
+     while kalkulator and arduino:
+          hasil = (((float(kalkulator) - float(arduino)) / float(kalkulator)) * 100)
           with open('hasil.txt', 'a') as f:
                f.write(str(hasil)+"\n")
 
-          line1 = f1.readline()
-          line2 = f2.readline()
+          # Baca baris selanjutnya
+          kalkulator = f1.readline()
+          arduino = f2.readline()
 
 
-
-# while True:
-#      kalkulator = float(input("Kalkulator : "))
-#      arduino = float(input("Arduino : "))
-#      persentase = 100
-#      hasil = ((kalkulator - arduino) / kalkulator) * persentase
-#      print(hasil)
+filename = 'hasil.txt'
+os.system(f'start notepad.exe {filename}')
