fd7a3f6516426067dd9956f87bad7ab4f4173959
Indra Wahyu <wicaksana0701@gmail.com>
2023-05-30 04:51:31 +0800

    update add multiplication feature

--- penyimpangan.py
+++ penyimpangan.py
@@ -1,18 +1,18 @@
 import os
 
 # Hapus data di hasil.txt
-with open('hasil.txt', 'w') as f:
+with open('penyimpangan/hasil.txt', 'w') as f:
   f.write('')
 
 # Buka file kalkulator.txt dan arduino.txt
-with open('kalkulator.txt', 'r') as f1, open('arduino.txt', 'r') as f2:
+with open('penyimpangan/kalkulator.txt', 'r') as f1, open('penyimpangan/arduino.txt', 'r') as f2:
      kalkulator = f1.readline()
      arduino = f2.readline()
 
      # Mulai perhitungan
      while kalkulator and arduino:
           hasil = (((float(kalkulator) - float(arduino)) / float(kalkulator)) * 100)
-          with open('hasil.txt', 'a') as f:
+          with open('penyimpangan/hasil.txt', 'a') as f:
                f.write(str(hasil)+"\n")
 
           # Baca baris selanjutnya
@@ -20,5 +20,5 @@ with open('kalkulator.txt', 'r') as f1, open('arduino.txt', 'r') as f2:
           arduino = f2.readline()
 
 
-filename = 'hasil.txt'
+filename = 'penyimpangan/hasil.txt'
 os.system(f'start notepad.exe {filename}')
